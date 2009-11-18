c748b70cad2752e58e8c380eda4c3d98caf4f5bb
Zenophon <mac75@case.edu>
2009-11-18 09:11:07 -0500

    Filled in ModelDataTest functions
    Signed-off-by: Zenophon <mac75@case.edu>

--- src/ModelDataTest.cpp
+++ src/ModelDataTest.cpp
@@ -9,22 +9,36 @@ namespace NUnitTests{
 
 	void ModelDataTest::load_MML()
 	{
+		ModelData ^data = gcnew ModelData();
+		data->load_MMLfile("test.xml");
+		Assert::AreEqual( 1 , 1 );
 	}
 
 	void ModelDataTest::load_XML()
 	{
+		ModelData ^data = gcnew ModelData();
+		data->load_XMLfile("test.xml");
+		Assert::AreEqual( 1 , 1 );
 	}
 
 	void ModelDataTest::load_var()
 	{
+		ModelData ^data = gcnew ModelData();
+		Assert::AreEqual( 1 , 1 );
 	}
 
 	void ModelDataTest::save_XML()
 	{
+		ModelData ^data = gcnew ModelData();
+		data->save_XMLfile("savetest.xml");
+		Assert::AreEqual( 1 , 1 );
 	}
 
 	void ModelDataTest::validate_eqn()
 	{
+		ModelData ^data = gcnew ModelData();
+		data->validate_equation("1 + 1 = 2 * x");
+		Assert::AreEqual( 1 , 1 );
 	}
 
 }
