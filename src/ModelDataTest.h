a0db8ed75bac4cdecb0bb9e8bc14c6932eac41b1
Barbie <barbarajoyjones@gmail.com>
2009-11-18 00:51:57 -0500

    added dependencies to project, started ModelDataTest

---
+++ src/ModelDataTest.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "ModelData.h"
+
+using namespace System;
+using namespace NUnit::Framework;
+
+namespace NUnitTests{
+
+	[TestFixture[
+	public ref class ModelDataTest
+	{
+	public:
+		ModelDataTest(void); // i think this needs to go away
+
+		[Test] void load_MML();
+		[Test] void load_XML();
+		[Test] void load_var();
+		[Test] void save_XML();
+		[Test] void validate_eqn();
+	};
+
+}
