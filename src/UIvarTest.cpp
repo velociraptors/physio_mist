2f290bfc71952abd85e839528d868b74bd04ee1d
Silvermoonnbeam <sjcummins18@gmail.com>
2009-11-17 23:59:57 -0700

    Since github hates me, here's my changes.

--- src/UIvarTest.cpp
+++ src/UIvarTest.cpp
@@ -24,9 +24,16 @@ namespace NUnitTests{
 
 	UIvarTest::change_Units()
 	{
+		String^ Units units = new String^ Units();
+		units.set(
 	}
 
 	UIvarTest::change_Value()
 	{
+		Value value = new Value();
+		//completely made up 11, maybe should change this?
+		value.set(11);
+		Assert::AreEqual(11, value.get());
 	}
+}
 }
