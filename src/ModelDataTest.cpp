50e024fd87653561b30d07ce206ebb1cdd5b2160
Barbie <barbarajoyjones@gmail.com>
2009-11-18 01:09:38 -0500

    more ModelDataTest skeleton

---
+++ src/ModelDataTest.cpp
@@ -0,0 +1,30 @@
+#include "StdAfx.h"
+#include "ModelDataTest.h"
+
+namespace NUnitTests{
+
+	ModelDataTest::ModelDataTest(void)
+	{
+	}
+
+	ModelDataTest::load_MML()
+	{
+	}
+
+	ModelDataTest::load_XML()
+	{
+	}
+
+	ModelDataTest::load_var()
+	{
+	}
+
+	ModelDataTest::save_XML()
+	{
+	}
+
+	ModelDataTest::validate_eqn()
+	{
+	}
+
+}
